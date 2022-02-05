82224657da95024a7c967d9ecd9ea297aabe7f96
TarsisSarabia <tarsisfabiano@gmail.com>
2022-02-05 14:33:25 -0300

    ChessException and ChessPosition

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -46,7 +46,7 @@ public class Board {
     }
 
     public boolean positionExists(int row, int column) {
-        return row >= 0 && row < rows && column >= 0 && column > columns;
+        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
     public boolean positionExists(Position position) {
