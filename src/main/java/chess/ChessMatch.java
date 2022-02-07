d403988bca1afceb602a33655c0dbcb078c328dc
TarsisSarabia <tarsisfabiano@gmail.com>
2022-02-06 21:33:02 -0300

    Pawn

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -4,6 +4,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 import java.util.ArrayList;
 import java.util.List;
@@ -186,19 +187,29 @@ public class ChessMatch {
 
     private void initialSetup() {
 
-        placeNewPiece('c', 1, new Rook(Color.WHITE, board));
-        placeNewPiece('c', 2, new Rook(Color.WHITE, board));
-        placeNewPiece('d', 2, new Rook(Color.WHITE, board));
-        placeNewPiece('e', 2, new Rook(Color.WHITE, board));
-        placeNewPiece('e', 1, new Rook(Color.WHITE, board));
-        placeNewPiece('d', 1, new King(Color.WHITE, board));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
-        placeNewPiece('c', 7, new Rook(Color.BLACK, board));
-        placeNewPiece('c', 8, new Rook(Color.BLACK, board));
-        placeNewPiece('d', 7, new Rook(Color.BLACK, board));
-        placeNewPiece('e', 7, new Rook(Color.BLACK, board));
-        placeNewPiece('e', 8, new Rook(Color.BLACK, board));
-        placeNewPiece('d', 8, new King(Color.BLACK, board));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
     }
 
 }
